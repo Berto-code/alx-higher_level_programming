285ea1f015ac803f5e34bde9889cf442bc226538
Berto-code <gilbertkamau32@gmail.com>
2023-11-03 10:02:05 +0300

    ff
    ff
    clear

--- 0x10-python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Send a JSON request
-curl -sXH POST "Content-Type: application" -d @"$2" "$1"
--- 0x10-python-network_0/4-header.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Displays the body of a response
-curl -s "$1" -X GET -H "X-School-User-Id: 98"
